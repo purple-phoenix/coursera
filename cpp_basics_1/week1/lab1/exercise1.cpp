39efa3d31e2006e12cab4d16835c69056ce1b806
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 13:27:42 -0500

    Exercise 1

---
+++ cpp_basics_1/week1/lab1/exercise1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  bool test = atoi((argv[1]));
+  
+  //add code below this line
+  bool my_bool;
+  my_bool = true;
+  cout << my_bool;
+  my_bool = false;
+  cout << my_bool;
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
