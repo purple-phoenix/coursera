e0590be3ad926b7201cc76d98e0cf597684492b8
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 14:29:22 -0500

    Add contents for exercise1

---
+++ cpp_basics_1/week2/exercise1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //fix the code below this line
+
+  int a = 4;
+  int b = 9;
+
+  //fix the code above this line
+  
+  cout << (a * 3 + b - 8 / 2) << endl;
+  
+  return 0;
+  
+}
