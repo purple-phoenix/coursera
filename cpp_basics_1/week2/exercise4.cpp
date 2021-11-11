ee2ef2629f23ffe60cd368365045ec544472335f
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 14:35:27 -0500

    Exercise 4

---
+++ cpp_basics_1/week2/exercise4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+  
+  double a = 7.0;
+  double b = 2;
+  cout << (a/b) << endl;
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
