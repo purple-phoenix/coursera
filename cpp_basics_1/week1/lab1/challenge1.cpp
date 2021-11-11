935d6a6c8b536bc5a0e841dc1f4bc128c4d8c446
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 13:09:26 -0500

    Lab1 Challenge 1

---
+++ cpp_basics_1/week1/lab1/challenge1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+  
+  string my_var = "I am learning"; //step 1
+  cout << my_var; //step 2
+  my_var = " to program"; //step 3
+  cout << my_var; //step 4
+  my_var = " in C++."; //step 5
+  cout << my_var << endl; //step 6
+  my_var = "Hooray!"; //step 7
+  cout << my_var; //step 8
+  
+  //add code above this line
+  
+  return 0;
+  
+}
+
