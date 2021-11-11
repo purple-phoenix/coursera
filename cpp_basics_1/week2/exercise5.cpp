ebb537b7f63ad96eed206fa8349fad6f0051f941
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 14:37:04 -0500

    Exercise 5 with bug

---
+++ cpp_basics_1/week2/exercise5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  string num1;
+  string num2;
+  cout << "Type the first whole number and then press Enter or Return: ";
+  cin >> num1;
+  cout << "Type the second whole number and then press Enter or Return: ";
+  cin >>num2;
+  
+  //fix the code below this line
+
+  string sum = num1 + num2;
+  cout << ( num1 + " + " + num2 + " = " + sum ) << endl;
+
+  //fix the code above this line
+  
+  return 0;
+  
+}
