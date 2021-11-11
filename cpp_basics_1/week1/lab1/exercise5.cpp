a99ffaeb6f1cf040d31a5f74d97def3ceba382df
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 13:34:51 -0500

    Exercise 5

---
+++ cpp_basics_1/week1/lab1/exercise5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  //add code below this line
+
+  cout << "Okay, it is time to learn";
+  cout << " about operators.";
+
+  //add code above this line
+  
+  return 0;
+  
+}
+
