023c01c13b31f5fd0dd5122329cc984395f2864d
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 13:14:17 -0500

    Challenge 2

---
+++ cpp_basics_1/week1/lab1/challenge2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+  int one = 1;
+  int two = 2;
+  int three = 3;
+  int four = 4;
+
+  two = one;
+  three = two;
+  four = three;
+
+  cout << four;
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
+
