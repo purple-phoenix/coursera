b121ed10125360b5301701d0c720e2dccc69d9e2
Matt McCarthy <matttmccarthy66@gmail.com>
2021-11-11 14:31:28 -0500

    Excercise 2

---
+++ cpp_basics_1/week2/exercise2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+  cout << boolalpha << (3 == 4);
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
